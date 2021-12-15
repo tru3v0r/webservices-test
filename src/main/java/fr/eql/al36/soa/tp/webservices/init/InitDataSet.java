7552e50835d01ae00412eb32c6c781b28da387c4
valvenom <valentin.venom@protonmail.com>
2021-12-15 14:38:21 +0100

    Injected Country objects at startup

--- src/main/java/fr/eql/al36/soa/tp/webservices/init/InitDataSet.java
+++ src/main/java/fr/eql/al36/soa/tp/webservices/init/InitDataSet.java
@@ -1,6 +1,8 @@
 package fr.eql.al36.soa.tp.webservices.init;
 
+import fr.eql.al36.soa.tp.webservices.entity.Country;
 import fr.eql.al36.soa.tp.webservices.entity.Currency;
+import fr.eql.al36.soa.tp.webservices.service.CountryService;
 import fr.eql.al36.soa.tp.webservices.service.CurrencyService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -10,16 +12,29 @@ import org.springframework.stereotype.Component;
 public class InitDataSet {
 
     private final CurrencyService currencyService;
+    private final CountryService countryService;
 
-    public InitDataSet(CurrencyService currencyService) {
+    public InitDataSet(CurrencyService currencyService, CountryService countryService) {
         this.currencyService = currencyService;
+        this.countryService = countryService;
         this.initData();
     }
 
     public void initData() {
-        currencyService.save(Currency.builder().ticker("EUR").name("Euro").value(1).build());
-        currencyService.save(Currency.builder().ticker("USD").name("Dollar").value(1.1).build());
-        currencyService.save(Currency.builder().ticker("GBP").name("Pound Sterling").value(0.9).build());
-        currencyService.save(Currency.builder().ticker("JPY").name("Japanese Yen").value(113).build());
+        Currency euro = Currency.builder().ticker("EUR").name("Euro").value(1).build();
+        Currency dollar = Currency.builder().ticker("USD").name("Dollar").value(1.1).build();
+        Currency pound = Currency.builder().ticker("GBP").name("Pound Sterling").value(0.9).build();
+        Currency yen = Currency.builder().ticker("JPY").name("Japanese Yen").value(113).build();
+        currencyService.save(euro);
+        currencyService.save(dollar);
+        currencyService.save(pound);
+        currencyService.save(yen);
+
+        countryService.save(Country.builder().code("FR").name("France").currency(euro).build());
+        countryService.save(Country.builder().code("DE").name("Germany").currency(euro).build());
+        countryService.save(Country.builder().code("ES").name("Spain").currency(euro).build());
+        countryService.save(Country.builder().code("US").name("United States of America").currency(euro).build());
+        countryService.save(Country.builder().code("GB").name("Great Britain").currency(euro).build());
+        countryService.save(Country.builder().code("JP").name("Japan").currency(euro).build());
     }
 }
